214b60b8b396e498f207ad667e8070b3696bfae9
Volodymyr Zherebetskyi <vzherebetskyi@gmail.com>
2020-02-11 09:57:36 +0200

    remove unnecessary data

--- src/app.js
+++ src/app.js
@@ -13,22 +13,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water bill', amount: 2500}));
-store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 10000}));
-// store.dispatch(setTextFilter('bill'));
-// store.dispatch(setTextFilter('Water'));
-
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('bill'));
-// },
-// 3000);
-
-const state = store.getState();
-const visible = getVisible(state.expenses, state.filters);
-
-console.log(visible);
-
 const jsx = (
     <Provider store = { store }>
         <AppRouter />
--- src/person.js
+++
@@ -1,6 +0,0 @@
-export const isAdult = (x) =>  x >= 18;
-
-export const canDrink = (x) => x >= 21;
-
-export default (x) => x >= 65;
-
